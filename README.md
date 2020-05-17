ce65f16470eca3c6bd858d41dd6cf7ea2989d440
arroyopablo <pabloandresarroyoparedes@gmail.com>
2020-05-17 10:58:04 -0500

    subiendo archivos

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mande -  Proyecto final bases de datos
+
+_Mande, es un emprendimiento que permite, desde el celular, conseguir personas trabajadoras, expertas y honestas para realizar a domicilio labores del día a día el hogar._
+
+## Comenzando 🚀 
+_Producto en desarrollo_
+### Instalación 🔧
---
+++ src/routes/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const {Client} = require('pg')
+
+
+const connectionData = {
+  user: 'postgres',
+  host: 'localhost',
+  database: 'DS1_BaseDeDatos',
+  password: 'cjloco1996',
+  port: 5432,
+}
+
+const client = new Client(connectionData)
+
+client.connect()
+client.query('SELECT nombre_persona FROM usuarios')
+    .then(response => {
+        console.log(response.rows)
+        client.end()
+    })
+    .catch(err => {
+        client.end()
+    })
+
+router.get('/', (req, res) => {
+  res.render('index', { title: 'First Web Node' });
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact Page' });
+});
+
+router.get('/login', (req, res) => {
+  res.render('login', { title: 'Acerca de' });
+});
+
+router.get('/login', (req, res) => {
+  res.render('login', { title: 'Inicio de sesión' });
+});
+
+router.get('/registro', (req, res) => {
+  res.render('registro', { title: 'Registro de usuario' });
+});
+
+module.exports = router;
